f1c45d16b179f6154d17a31770e840e44bb1d354
Lukasz Gawron <51411768+LUKIS123@users.noreply.github.com>
2023-08-01 16:45:06 +0200

    pr

--- Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Package_System_CRUD;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
